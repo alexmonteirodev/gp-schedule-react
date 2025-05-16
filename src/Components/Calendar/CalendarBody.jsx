392f3a0cca7c58cd53770971096a46fb989b1335
alexmonteirodev <159573548+alexmonteirodev@users.noreply.github.com>
2025-05-16 10:24:35 +0200

    dia atual fix

--- src/Components/Calendar/CalendarBody.jsx
+++ src/Components/Calendar/CalendarBody.jsx
@@ -23,29 +23,34 @@ const CalendarBody = () => {
     daysArray.push(i);
   }
 
-  console.log(today);
   return (
     <div className="grid grid-cols-7 mt-3">
-      {daysArray.map((day, i) => (
-        <div
-          key={i}
-          className={`h-20 text-center font-bold text-[1.10rem] border-t-1 border-gray-200 relative pt-1 ${
-            day === currentDate ? " text-white" : ""
-          }`}
-        >
-          {day}
-          {day === currentDate && (
-            <div className="size-7 z-[-1] top-[.1875rem] absolute left-1/2 -translate-x-1/2 rounded-full bg-gray-600"></div>
-          )}
-          {/* {day != null ? (
+      {daysArray.map((day, i) => {
+        const isToday =
+          day === currentDate &&
+          currentMonth === today.getMonth() &&
+          currentYear === today.getFullYear();
+        return (
+          <div
+            key={i}
+            className={`h-20 text-center font-bold text-[1.10rem] border-t-1 border-gray-200 relative pt-1 ${
+              isToday ? " text-white" : ""
+            }`}
+          >
+            {day}
+            {isToday && (
+              <div className="size-7 z-[-1] top-[.1875rem] absolute left-1/2 -translate-x-1/2 rounded-full bg-gray-600"></div>
+            )}
+            {/* {day != null ? (
             <p className="h-4 mt-1 w-11 m-auto bg-yellow-400 rounded-md border-1 border-gray-300 text-[0.6rem] font-medium">
               Morning
             </p>
           ) : (
             ""
           )} */}
-        </div>
-      ))}
+          </div>
+        );
+      })}
     </div>
   );
 };
